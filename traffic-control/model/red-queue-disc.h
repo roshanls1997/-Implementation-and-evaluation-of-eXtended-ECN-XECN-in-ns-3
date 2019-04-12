6ab67257602634199d8ed61040548c04a535ecef
Ashrit26 <ashritmr@rocketmail.com>
2019-04-12 20:55:49 +0530

    Update red-queue-disc.h

--- traffic-control/model/red-queue-disc.h
+++ traffic-control/model/red-queue-disc.h
@@ -19,6 +19,12 @@
  *
  * Thanks to: Duy Nguyen<duy@soe.ucsc.edu> by RED efforts in NS3
  *
+ 
+ Team Members
+Ashrit (16CO122)
+Darshan (16CO236)
+Roshan (16CO242)
+Shashank (16CO246)
  *
  * This file incorporates work covered by the following copyright and
  * permission notice:
